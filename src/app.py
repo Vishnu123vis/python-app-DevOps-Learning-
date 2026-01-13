1d7b32ddc609e99013f966c95fb8f9f9808151ff
Vishnu Subramani <vishsubramani3@gmail.com>
2026-01-13 16:31:36 -0500

    First Commit

---
+++ src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from datetime import datetime
+import socket
+app = Flask(__name__)
+
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify ({
+        'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'hostname': socket.gethostname()
+    })
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({
+        'status': 'ok'
+    }), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
+
+
+
+
+#'/api/v1/details'
+#'/api/v1/healthz'
