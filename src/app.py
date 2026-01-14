2cae8f8b96ba78f4a05c0fac4e34ab51c7f521b9
Vishnu Subramani <vishsubramani3@gmail.com>
2026-01-14 12:36:08 -0500

    Adds ci github workflow

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ def details():
     return jsonify ({
         'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'hostname': socket.gethostname()
+        'message': 'You are doing great!'
     })
 
 @app.route('/api/v1/healthz')
