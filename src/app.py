2842d3e35a62ec8d02cd72d522ca8a4765372363
Vishnu Subramani <vishsubramani3@gmail.com>
2026-01-15 14:43:21 -0500

    Adds argocd installation

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify ({
         'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'hostname': socket.gethostname(),
-        'message': 'You are doing great! HIsdasdasdIHDasjdiasjdaIASHDISA!!Jaiojsdiodasdasdajsd'
+        'message': 'You are doing great! HIsdasdasdIHDasjdiasjddasdasdjaaIASHDISA!!Jaiojsdiodasdasdajsd'
     })
 
 @app.route('/api/v1/healthz')
