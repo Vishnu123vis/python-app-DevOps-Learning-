797c3f36f35709f2da0f4c38a2e4204329e8269b
Vishnu Subramani <vishsubramani3@gmail.com>
2026-01-15 16:14:10 -0500

    Aadds new info endpoint

--- src/app.py
+++ src/app.py
@@ -4,13 +4,14 @@ import socket
 app = Flask(__name__)
 
 
-@app.route('/api/v1/details')
+@app.route('/api/v1/info')
 
-def details():
+def info():
     return jsonify ({
         'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'hostname': socket.gethostname(),
-        'message': 'You are doing great! !!Jaiojasdasdsdiodasdadasdsdajsd'
+        'message': 'You are doing great! !!Jaiojasdasdsdiodasdadasdsdajsd',
+        'deployed_on': 'kubernetes'
     })
 
 @app.route('/api/v1/healthz')
