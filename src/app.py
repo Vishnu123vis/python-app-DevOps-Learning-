53e5c10830b07cf66b1f78685dfcc745b3c9dc20
Vishnu Subramani <vishsubramani3@gmail.com>
2026-01-15 13:50:41 -0500

    Adds CD job

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify ({
         'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'hostname': socket.gethostname(),
-        'message': 'You are doing great! HIIIHDIASHDISA!!Jaiojsdioajsd'
+        'message': 'You are doing great! HIIIHDIASHDISA!!Jaiojsdiodasdasdajsd'
     })
 
 @app.route('/api/v1/healthz')
